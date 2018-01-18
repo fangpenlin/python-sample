af089428da0e91bf6ba2bc5a68592d0f796b7fe2
Fang-Pen Lin <hello@fangpenlin.com>
2018-01-18 11:31:39 -0800

    fix port issue

--- app.py
+++ app.py
@@ -18,4 +18,4 @@ if __name__ == '__main__':
     app = web.Application()
     app.router.add_get('/loading.gif', loading_gif)
     app.router.add_get('/longcat.jpg', longcat)
-    web.run_app(app, port=os.getenv('PORT', 5000))
+    web.run_app(app, port=int(os.getenv('PORT', 5000)))
